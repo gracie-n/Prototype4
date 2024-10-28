1b780a94d4feb40b6782ce8995d5eaceac15aa80
Grace N <gracelouisen07@gmail.com>
2024-10-28 09:01:55 -0700

    Create with Code 4.4

--- Assets/Scripts/spawnManager.cs
+++ Assets/Scripts/spawnManager.cs
@@ -5,20 +5,36 @@ using UnityEngine;
 public class spawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnRange = 9; 
+    private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab; 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
 
-       Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<enemy>().Length; 
+        if (enemyCount == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.position);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            
+
+        }
+       
+        if (transform.position.y <-10)
+        {
+            Destroy(gameObject); 
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -27,5 +43,15 @@ public class spawnManager : MonoBehaviour
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos; 
+
+        
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
